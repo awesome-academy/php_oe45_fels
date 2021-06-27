a1287dfbd4061418a4e912f838a74d1f92076efd
TrungTran <quoctrung.tran210@gmail.com>
2021-06-27 23:20:49 +0700

    Done Register and Login,User's  Migration, Model , Controller Auth

---
+++ resources/lang/en/field.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Name' => 'Name',
+    'Register' => 'Register',
+    'Login' => 'Login',
+    'E-Mail Address' => 'E-Mail Address',
+    'Password' => 'Password',
+    'Confirm Password' => 'Confirm Password',
+];
