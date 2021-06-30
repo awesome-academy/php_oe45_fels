42714b72cecd53e8661ad8e9ad3d23979b918c54
Thanhloc253 <thanhloc@ubuntu.ubuntu-domain>
2021-06-30 09:36:02 +0700

    create controller, i18n, view home, topics page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,8 +69,8 @@
                 </div>
             </div>
         </nav>
-            <a href="{!! route('user.change-language', ['en']) !!}">{{ trans('home.english') }}</a>
-            <a href="{!! route('user.change-language', ['vi']) !!}">{{ trans('home.vietnam') }}</a>
+            <a href="{!! route('user.change-language', ['en']) !!}">{{ trans('field.english') }}</a>
+            <a href="{!! route('user.change-language', ['vi']) !!}">{{ trans('field.vietnam') }}</a>
                 <main class="py-4">
                     @yield('content')
                 </main>
